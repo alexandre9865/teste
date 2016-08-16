e59b5268856bcee48f9cc5ed4a1478e7c2cf7190
alexandre9865 <alexandremontebelo@yahoo.com.br>
2016-08-16 20:23:44 -0300

    Criação tela da seleção
    Tela para selecionar a imagem

--- demo.py
+++ demo.py
@@ -5,26 +5,32 @@ from PySide.QtCore import *
 qt_app = QApplication(sys.argv)
  
 class LayoutExample(QWidget):
-	''' An example of PySide/PyQt absolute positioning; the main window
-		inherits from QWidget, a convenient widget for an empty window. '''
- 
+
+	
+	
 	def __init__(self):
 		# Initialize the object as a QWidget and
 		# set its title and minimum width
 		QWidget.__init__(self)
-		self.setWindowTitle('Dynamic Greeter')
+		self.setWindowTitle('Show, Image!')
 		self.setMinimumWidth(400)
  
 		# Create the QVBoxLayout that lays out the whole form
 		self.layout = QVBoxLayout()
- 
+		self.setLayout(self.layout)
+
 		# Create the form layout that manages the labeled controls
-		self.form_layout = QFormLayout()
- 
+		
+		self.widget1 = QWidget()
+		self.layout.addWidget(self.widget1)
+
+		self.image_layout = QFormLayout()
+		self.widget1.setLayout(self.image_layout)
+
 		# The salutations that we want to make available
 		self.options = ['building.jpg',
-							'cat.png',
-							'planet.jpg']
+						'cat.png',
+						'planet.jpg']
  
 		# Create and fill the combo box to choose the salutation
 		self.option = QComboBox(self)
@@ -32,31 +38,39 @@ class LayoutExample(QWidget):
  
 		# Add it to the form layout with a label
 		self.label = QLabel() 
-		self.pixmap = QPixmap('C:/Users/Alexandre/Documents/GitHub/teste/building.jpg')
+		self.pixmap = QPixmap('C:/Users/Alexandre/Documents/GitHub/teste/cat.png')
 		self.label.setPixmap(self.pixmap)
-		 
+
+		self.button = QPushButton('Configurations', self)
+		self.button.clicked.connect(lambda: self.alternate(1==0))
 		# Add it to the form layout with a label
-		self.form_layout.addRow(self.label)
-		self.form_layout.addRow(self.option)
+		self.image_layout.addRow(self.label)
+		self.image_layout.addRow(self.button)
+
+		self.widget2 = QWidget()
+		self.layout.addWidget(self.widget2)
+
+		self.config_layout = QVBoxLayout()
+		self.widget2.setLayout(self.config_layout)
+
+		self.buttonSave = QPushButton('Save', self)
+		self.buttonSave.clicked.connect(lambda: self.save())
+
+		self.config_layout.addWidget(self.option)
+		self.config_layout.addStretch(1)
+		self.config_layout.addWidget(self.buttonSave)
   
 		# Add the form layout to the main VBox layout
-		self.layout.addLayout(self.form_layout)
+		self.layout.addLayout(self.config_layout)
  
 		# Add stretch to separate the form layout from the button
 		self.layout.addStretch(1)
- 
-		# Create a horizontal box layout to hold the button
-		self.button_box = QHBoxLayout()
- 
-		# Add stretch to push the button to the far right
-		self.button_box.addStretch(1)
- 
-		# Add the button box to the bottom of the main VBox layout
-		self.layout.addLayout(self.button_box)
- 
-		# Set the VBox layout as the window's main layout
-		self.setLayout(self.layout)
- 
+	
+	def alternate(self, status):
+		self.widget1.setVisible(status)
+		self.widget2.setVisible(not status)
+
+
 	def run(self):
 		# Show the form
 		self.show()
